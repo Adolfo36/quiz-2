9aebe6f4f311e711007ebe7a243aa7765f6dba11
Adolfo Cortell <adolfo.c36@gmail.com>
2015-08-03 19:29:22 +0200

    Final modulo 7 sin el ejercicio

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });	// modificado acm
 });
-
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/author/creditos', quizController.creditos);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);	// autoload : quizId
+// Definición de rutas de /quizes
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/author/creditos', 				quizController.creditos);
 
 module.exports = router;
